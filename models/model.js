d1d8ecd8405ef049dec131338f876aec9da667aa
lqhuyhust <lqhuy.hust@gmail.com>
2020-12-08 15:12:53 +0700

    thay board-->project

--- models/model.js
+++ models/model.js
@@ -4,109 +4,109 @@
     window.app.Model = Model
 
     function Model() {
-        this.boards = []
+        this.projects = []
     }
 
     Model.prototype.uid = function () {
         return Array(5).fill().map(i => Math.floor(Math.random() * (10 - 0)) + 0).join('')
     }
 
-    Model.prototype.addBoard = function (board) {
-        this.boards.push(board)
+    Model.prototype.addProject = function (project) {
+        this.projects.push(project)
     }
 
-    Model.prototype.addList = function (boardID, list) {
-        this.getBoard(boardID).lists.push(list)
+    Model.prototype.addList = function (projectID, list) {
+        this.getProject(projectID).lists.push(list)
     }
 
     Model.prototype.addItem = function (listID, item) {
         this.getList(listID).items.push(item)
     }
 
-    Model.prototype.insertItem = function (listID, item, position){
+    Model.prototype.insertItem = function (listID, item, position) {
         this.getList(listID).items = this.getList(listID).items.slice(0, position).concat(item).concat(this.getList(listID).items.slice(position))
     }
 
-    Model.prototype.getBoards = function () {
-        return this.boards
+    Model.prototype.getProjects = function () {
+        return this.projects
     }
 
-    Model.prototype.getBoard = function (boardID) {
-        return this.boards.find(board => board.id == boardID)
+    Model.prototype.getProject = function (projectID) {
+        return this.projects.find(project => project.id == projectID)
     }
 
     Model.prototype.getList = function (listID) {
-        return this.getListBoard(listID).lists.find(list => list.id == listID)
+        return this.getListProject(listID).lists.find(list => list.id == listID)
     }
 
-    Model.prototype.getListBoard = function (listID) {
-        return this.boards.find(board => board.lists.find(list => list.id == listID))
+    Model.prototype.getListProject = function (listID) {
+        return this.projects.find(project => project.lists.find(list => list.id == listID))
     }
 
     Model.prototype.getItem = function (itemID) {
         return this.getItemList(itemID).items.find((item) => item.id == itemID)
     }
-    
+
     Model.prototype.getItemList = function (itemID) {
         let itemList
-        this.boards.forEach(board => {
-            board.lists.forEach(list => {
-                list.items.forEach(item  => {
-                    if(item.id == itemID){
+        this.projects.forEach(project => {
+            project.lists.forEach(list => {
+                list.items.forEach(item => {
+                    if (item.id == itemID) {
                         itemList = list
-                    }      
+                    }
                 })
             })
         })
         return itemList
     }
 
-    Model.prototype.updateBoardTitle = function (boardID, title){
-        this.getBoard(boardID).title = title
+    Model.prototype.updateProjectTitle = function (projectID, title) {
+        this.getProject(projectID).title = title
     }
 
-    Model.prototype.updateListTitle = function (listID, title){
+    Model.prototype.updateListTitle = function (listID, title) {
         this.getList(listID).title = title
     }
 
-    Model.prototype.updateItemTitle = function (itemID, title){
+    Model.prototype.updateItemTitle = function (itemID, title) {
         this.getItem(itemID).title = title
     }
-        
-    Model.prototype.removeBoard = function (boardID) {
-        this.boards.splice(this.getBoardIndex(boardID), 1)
+
+    Model.prototype.removeProject = function (projectID) {
+        this.projects.splice(this.getProjectIndex(projectID), 1)
     }
 
     Model.prototype.removeList = function (listID) {
-        this.getListBoard(listID).lists.splice(this.getListIndex(listID), 1)
+        this.getListProject(listID).lists.splice(this.getListIndex(listID), 1)
     }
 
     Model.prototype.removeItem = function (itemID) {
         this.getItemList(itemID).items.splice(this.getItemIndex(itemID), 1)
     }
 
-    Model.prototype.getBoardIndex = function (boardID) {
-        return this.boards.findIndex(board => board.id === boardID)
+    Model.prototype.getProjectIndex = function (projectID) {
+        return this.projects.findIndex(project => project.id === projectID)
     }
 
     Model.prototype.getListIndex = function (listID) {
-        return this.getListBoard(listID).lists.findIndex(list => list.id == listID)
+        return this.getListProject(listID).lists.findIndex(list => list.id == listID)
     }
 
     Model.prototype.getItemIndex = function (itemID) {
         return this.getItemList(itemID).items.findIndex((item) => item.id == itemID)
     }
-/*
-    Model.prototype.getBoardsCount = function () {
-        return this.boards.length
-    }
-
-    Model.prototype.getListsCount = function () {
-        return this.boards.reduce((sum, board) => sum += board.lists.length, 0)
-    }
-
-    Model.prototype.getItemsCount = function () {
-        return this.boards.reduce((sum, board) => sum += board.lists.reduce((sumI, list) => sumI += list.items.length, 0), 0)
-    }
-*/
+    /*
+        Model.prototype.getProjectsCount = function () {
+            return this.projects.length
+        }
+    
+        Model.prototype.getListsCount = function () {
+            return this.projects.reduce((sum, project) => sum += project.lists.length, 0)
+        }
+    
+        Model.prototype.getItemsCount = function () {
+            return this.projects.reduce((sum, project) => sum += project.lists.reduce((sumI, list) => sumI += list.items.length, 0), 0)
+        }
+    */
 })()
--- templates/template.js
+++ templates/template.js
@@ -1,8 +1,8 @@
 
 window.app = window.app || {}
-window.app.Templates = { Login, Home, Board, Modal }
+window.app.Templates = { Login, Home, Project, Modal, ProjectModal }
 
-function Login () {
+function Login() {
     return `
     <main class="vertical-center">
         <div class="border-radius shadow w-50 auto-margin">
@@ -45,44 +45,43 @@ function Login () {
     `
 }
 
-function Home (boards) {
+function Home(projects) {
     return `<header class="shadow header fixed" style="height: 56px">
                 <h3 class="no-margin">Trello Clone</h3>
             </header>
             <main class="vertical-center">
                 <div class="border-radius shadow bg-white w-50 auto-margin">
                     <div class="flex border-bottom padding">
-                        <h3 class="no-margin">Boards</h3>
-                        <button id="add-board" class="icon-button color-success"><i class="lnr lnr-plus-circle"></i></button>
+                        <h3 class="no-margin">Projects</h3>
+                        <button id="add-project" class="icon-button color-success"><i class="lnr lnr-plus-circle"></i></button>
                     </div>
-                    ${  
-                        boards.length !== 0 ? 
-                        boards.reduce((acc, board) => acc += BoardButton(board), '') : 
-                        '<div class="padding color-gray text-center">No boards</div>'
-                    }
+                    ${projects.length !== 0 ?
+            projects.reduce((acc, project) => acc += ProjectButton(project), '') :
+            '<div class="padding color-gray text-center">No projects</div>'
+        }
                     <div class="padding border-top"></div>
                 </div>
             </main>`
 }
 
-function BoardButton ({id, title}) {
+function ProjectButton({ id, title }) {
     return `<div class="flex list-button">
-                <button id="remove-board-${id}" class="icon-button color-danger remove-board">
+                <button id="remove-project-${id}" class="icon-button color-danger remove-project">
                     <i class="lnr lnr-cross"></i>
                 </button>
-                <button id="open-board-${id}" class="padding flex board-button"> 
+                <button id="open-project-${id}" class="padding flex project-button"> 
                     ${title}
                     <i class="lnr lnr-chevron-right"></i>
                 </button>
             </div>`
 }
 
-function Board ({id, title, lists}) {
+function Project({ id, title, lists }) {
     return `<header class="header shadow">
                 <div class="flex">
                     <button id="back-page" class="icon-button color-gray back-button"><i class="lnr lnr-chevron-left"></i></button>
-                    <h3 id="board-title" class="no-margin show">${title}</h3>
-                    <input type="text" id="board-title-input" class="hide" value="${title}" placeholder="Title">
+                    <h3 id="project-title" class="no-margin show">${title}</h3>
+                    <input type="text" id="project-title-input" class="hide" value="${title}" placeholder="Title">
                 </div>
                 <div>
                     <button id="help-button" class="icon-button color-info"><i class="lnr lnr-question-circle"></i></button>
@@ -91,12 +90,12 @@ function Board ({id, title, lists}) {
             <main class="padding row">
                 ${lists.reduce((acc, list) => acc += List(list), '')}
                 <div>
-                    <button id="add-list" board="${this.id}" class="icon-button"><i class="lnr lnr-plus-circle"></i></button>
+                    <button id="add-list" project="${this.id}" class="icon-button"><i class="lnr lnr-plus-circle"></i></button>
                 </div>
             </main>`
 }
 
-function List ({id, title, items}) {
+function List({ id, title, items }) {
     return `<div class="list bg-white shadow border-radius">
                 <div class="header border-bottom">
                     <h4 id="list-${id}-title" class="no-margin show list-title">${title}</h4>
@@ -110,14 +109,14 @@ function List ({id, title, items}) {
             </div>`
 }
 
-function Item ({id, title}) {
+function Item({ id, title }) {
     return `<div id="item-${id}-title" class="item show droppable" draggable="true">
                 <p class="no-margin border border-radius">${title}</p>
             </div>
             <textarea id="item-${id}-title-input" class="hide border border-radius item-input">${title}</textarea>`
 }
 
-function Modal (title, text, hasButtons) {
+function Modal(title, text, hasButtons) {
     return `<div class="modal-container">
                 <div id="modal-bg"></div>
                 <div class="modal border-radius shadow bg-white">
@@ -132,4 +131,64 @@ function Modal (title, text, hasButtons) {
                     </div>
                 </div>
             </div>`
+}
+
+function ProjectModal(title) {
+    return `
+    <div class="modal-container">
+        <div id="project-modal-bg"></div>
+
+        <div class="modal border-radius shadow bg-white">
+            <div class="padding modal-header">
+                <h4 class="text-center no-margin">${title}</h4>
+                <button id="close-modal" class="icon-button color-danger close-modal">
+                    <i class="lnr lnr-cross"></i>
+                </button>
+            </div>
+            <div class="padding">
+                <div class="project-form">
+                    <label>Project Name: </label>
+                    <input name="p_name" id="p_name">
+                </div>
+            </div>
+            <div class="padding modal-submit">
+                <button id="modal-submit">Submit</button>
+            </div>
+            
+        </div>
+    </div>
+    `
+}
+
+function TaskModal(title) {
+    return `
+    <div class="modal-container">
+        <div id="task-modal-bg"></div>
+
+        <div class="modal border-radius shadow bg-white">
+            <div class="padding modal-header">
+                <h4 class="text-center no-margin">${title}</h4>
+                <button id="close-modal" class="icon-button color-danger close-modal">
+                    <i class="lnr lnr-cross"></i>
+                </button>
+            </div>
+            <div class="padding">
+                <div class="task-form">
+                    <div class="task-name">
+                        <label>Task Name: </label>
+                        <input name="p_name" id="p_name">
+                    </div>
+                    <div class="task-user">
+                        <label>Assignee: </label>
+                        <select class="asigned-user">
+                            <option id="1">User 1</option>
+                            <option id="2">User 2</option>
+                            <option id="3">User 3</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
 }
